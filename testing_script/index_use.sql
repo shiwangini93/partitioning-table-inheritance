3f2758a97760989149f4de740ddbd0f5185bf3f8
gini <shishulkargini@gmail.com>
2023-11-19 01:16:28 +0530

    adding test scripts

---
+++ testing_script/index_use.sql
@@ -0,0 +1,8 @@
+-- Use below query to validate whether index(round_amount_idx_partition_name) is getting used for not
+
+explain SELECT id, account_id , amount,  created_datetime 
+FROM transactions
+WHERE account_id = '1e3b2691-43bb-4825-b0b1-85380be9512d'
+  AND amount::numeric % 1 = 0  -- Checks for perfectly round amounts
+ORDER BY created_datetime  DESC  -- Considers the last transactions
+LIMIT 10; 
